4c6cebf66dba2ac8488315b1ce1f962159ac3e8a
mweiss <michael.josh.weiss@gmail.com>
2016-08-11 22:03:55 +0800

    you can now save your user name

--- index.js
+++ index.js
@@ -4,37 +4,59 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var randomWords = require('random-words');
 
+/**
+ * Expose the public directory.
+ */
 app.use('/public', express.static('public'));
 
+/**
+ * If you go to the base site, we'll auto generate a chat room for you.
+ */
 app.get('/', function(req, res){
   res.redirect('/c/' + randomWords({exactly: 2, join: '-'}));
 });
 
+/**
+ *  This initializes a chat room for the given URL.  Chatrooms are prefixed with /c/:roomName
+ */
 app.get('/c/:roomName', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket, testOptions){
-  console.log('connected!');
+/**
+ * Helper method to log messages with the socket id.
+ */
+function logWithSocket(socket, msg) {
+  console.log('[' + socket.id + ']: ' + msg);
+}
+
+/**
+ * Add listeners when the user connects.  The user will first tell the server
+ * what room it's in so we can correctly filter messages.
+ */
+io.on('connection', function(socket){
+  logWithSocket(socket, 'Connected!');
   socket.broadcast.emit('hi');
 
   var room;
 
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    logWithSocket(socket, 'message: ' + msg);
     if (room) {
       io.to(room).emit('chat message', msg);
     }
   });
+
   socket.on('subscribe to room', function(r) {
     room = r;
     socket.join(room);
-  })
+  });
+
 });
+
+/**
+ *  Start the server.
+ */
 var port = (process.env.PORT || 5000);
 http.listen(port, function(){
   console.log('listening on *:' + port);
--- public/client.js
+++ public/client.js
@@ -3,19 +3,39 @@
   var roomName = pathParts[pathParts.length - 1];
   var socket = io();
 
+  // Let the server know what room we're in.
   socket.emit('subscribe to room', roomName)
+
+  var anonName = 'Anonymous' + Math.floor((Math.random() * 100000000) + 1);
+  $('#name').val(Cookies.get('name') || anonName)
+
+  function getName() {
+    return $('#name').val() || anonName
+  }
+
+  $('#name').on('change', function() {
+    Cookies.set('name', getName());
+  });
 
   $('form').submit(function(){
     console.log('sending chat message', $('#m').val())
-    socket.emit('chat message', $('#m').val());
+    socket.emit('chat message', {message: $('#m').val(), name: getName(), id: socket.io.engine.id});
     $('#m').val('');
     return false;
   });
+
+  // When we receive a message, append it to the list of messages.
   socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    var isMe = socket.io.engine.id === msg.id;
+    var name = isMe ? "Me" : msg.name;
+
+    var nameEle = $('<span class="name">').text(name + ':');
+    var messageEle = $('<span class="msg">').text(msg.message);
+
+    $('#messages').append($('<li>').append(nameEle).append(messageEle));
   });
 
-  // Let's append a message so we can invite others.
+  // We'll append a message to the beginning of the chat so the user can invite others.
   var href = window.location.href;
   var subject = encodeURIComponent('Let\'s chat!')
   var body = encodeURIComponent('Hey, click this link so we can chat: ' + href);
